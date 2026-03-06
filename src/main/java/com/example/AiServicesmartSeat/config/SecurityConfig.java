143cc303929a781a92b94c93888f4c899b8e0a93
Bg <bhavyagodhaniya2004@gmail.com>
2026-03-06 12:40:57 +0530

    ....

--- src/main/java/com/example/AiServicesmartSeat/config/SecurityConfig.java
+++ src/main/java/com/example/AiServicesmartSeat/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+
                 .headers(headers -> headers
                         .frameOptions(frame -> frame.disable()) // Typical for proxies
                 )
