123bde9bf437bc290db5f68d2b210bd13c6f0839
Bg <bhavyagodhaniya2004@gmail.com>
2026-04-07 16:06:00 +0530

    cors fix2

--- src/main/java/com/example/AiServicesmartSeat/util/JwtAuthenticationFilter.java
+++ src/main/java/com/example/AiServicesmartSeat/util/JwtAuthenticationFilter.java
@@ -85,6 +85,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             boolean isSeb = (userAgent != null && userAgent.contains("SEB"));
             boolean isKeyValid = (sebConfigKey != null && sebConfigKey.equals(requestKey));
 
+            System.out.println("from UI req key = "+requestKey);
+            System.out.println("isseb = "+isSeb);
+            System.out.println("isvalid = "+isKeyValid);
+            System.out.println("key in server = "+sebConfigKey);
             if (!isSeb || !isKeyValid) {
                 // Perform Server-side Logout
                 authService.logout(response);
